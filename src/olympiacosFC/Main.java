f655adc8a12b245cc45a5d6d4cc9f266d6156dbe
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-08-27 16:50:16 +0300

    add day 1

--- src/olympiacosFC/Main.java
+++ src/olympiacosFC/Main.java
@@ -1,5 +1,7 @@
 package olympiacosFC;
 
+import static olympiacosFC.Players.teamTotals;
+
 /**
  * Keeps goals and assists records for Olympiacos FC for season 2025-2026.
  * --
@@ -9,4 +11,25 @@ package olympiacosFC;
  */
 
 public class Main {
+
+    static int matchNumber = 0;
+
+    public static void main(String[] args) {
+
+        System.out.printf("Match number: %02d | Olympiacos 2 - 0 Asteras Tripolis (23/08/25)%n", ++matchNumber);
+        System.out.println("1-0: Yazici (Mouzakitis), 2-0: El Kaabi (Nascimento)\n");
+
+        Players Yazici = new Players("Yazici", 97, "Yusuf", "Türkiye", 1997);
+        Players Mouzakitis = new Players("Mouzakitis", 96, "Christos", "Greece", 2006);
+        Players ElKaabi = new Players("El Kaabi", 9, "Ayoub", "Morocco", 1993);
+        Players Nascimento = new Players("Nascimento", 8, "Diogo", "Portugal", 2002);
+
+        Yazici.addGoal(1); Mouzakitis.addAssist(1); ElKaabi.addGoal(1); Nascimento.addAssist(1);
+
+        System.out.println(Yazici.playerDetails());
+        System.out.println(Nascimento.playerDetails());
+
+        System.out.println(teamTotals());
+
+    }
 }
--- src/olympiacosFC/Players.java
+++ src/olympiacosFC/Players.java
@@ -1,4 +1,108 @@
 package olympiacosFC;
 
 public class Players {
+    private static int totalGoals = 0;
+    private static int totalAssists = 0;
+
+    private String lastname;
+    private int shirtNumber;
+
+    private String firstname;
+    private String middlename;
+    private String nationality;
+    private int birthYear;
+
+    protected int goals;
+    protected int assists;
+
+    public Players() {
+
+    }
+
+    public Players(String lastname, int shirtNumber) {
+        this.lastname = lastname;
+        this.shirtNumber = shirtNumber;
+    }
+
+    public Players(String lastname, int shirtNumber, String firstname, String nationality, int birthYear) {
+        this.lastname = lastname;
+        this.shirtNumber = shirtNumber;
+        this.firstname = firstname;
+        this.nationality = nationality;
+        this.birthYear = birthYear;
+        goals = 0;
+        assists = 0;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setShirtNumber(int shirtNumber) {
+        this.shirtNumber = shirtNumber;
+    }
+
+    public int getShirtNumber() {
+        return this.shirtNumber;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = this.firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getMiddlename() {
+        if (middlename == null) return "-";
+        return middlename;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void addGoal(int g) {
+        if (g >= 1) goals += g;
+        totalGoals += g;
+    }
+
+    public void addAssist(int a) {
+        if (a >= 1) assists += a;
+        totalAssists += a;
+    }
+
+    public String playerDetails() {
+        if (middlename != null) {
+            return String.format("Player: %s %s %s [%d], from %s born in %d.%nStats: Goals: %d | Assists: %d.%n",
+                    lastname, middlename, firstname, shirtNumber, nationality, birthYear, goals, assists);
+        }
+        return String.format("Player: %s %s [%d], from %s born in %d.%nStats: Goals: %d | Assists: %d.%n",
+                lastname, firstname, shirtNumber, nationality, birthYear, goals, assists);
+    }
+
+    static String teamTotals() {
+        return "Total Goals: " + totalGoals + " | Total Assists: " + totalAssists + "\n";
+    }
 }
